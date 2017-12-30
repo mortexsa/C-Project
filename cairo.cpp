df8770a0d649654f048b53c5e015dfb2a5d5e2f4
Thunder_9 <tristan.bessac@ens.uvsq.fr>
2017-12-30 22:35:37 +0100

    correction julia

--- cairo.cpp
+++ cairo.cpp
@@ -40,7 +40,7 @@ void Cairo::dessiner_M(){
 	
 }
 
-void Cairo::dessiner_J(){
+void Cairo::dessiner_J(std::complex<double> c){
 	  cairo_surface_t *surface;
   cairo_t *cr;
 	int zoom=1/this->getGranularite();
@@ -55,7 +55,7 @@ void Cairo::dessiner_J(){
 	cairo_set_source_rgb(cr, 0, 0, 0);
     for(x=0; x<imagex; x+=1){
 		for(y=0; y<imagey; y+=1){
-			int i=this->algoJuliaFatou(x,y,this->getModMax(),100);
+			int i=this->algoJuliaFatou(x,y,this->getModMax(),100,c);
 				cairo_set_source_rgb(cr, 0, 0, i*255/100);
 				cairo_rectangle(cr,x,y,1,1);
 				cairo_fill (cr);
--- cairo.hh
+++ cairo.hh
@@ -9,8 +9,8 @@
 class Cairo : public dessin{
 		public:
 		Cairo(const double x_min, const double x_max, const double y_min, const double y_max, const double z_max);
-		void dessiner_M(double x, double y, int i);
-		void dessiner_J(double x, double y, int i);
+		void dessiner_M();
+		void dessiner_J(std::complex<double> c);
 		~Cairo();
 }
 
