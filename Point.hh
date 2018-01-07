81b3f43309a4bba1b6c562c2de053c2b21facd3a
Thunder_9 <tristan.bessac@ens.uvsq.fr>
2018-01-07 22:04:35 +0100

    doxygen pour point et Glwidget

--- Point.hh
+++ Point.hh
@@ -1,25 +1,71 @@
 #ifndef POINT_HH
 #define POINT_HH
 
+
+/*!
+ *  \file Point.hh
+ * \brief class Point 
+ */
+ 
+ 
+//! \struct color
+/*! \brief objet la couleur d'un point 
+la structure represente une couleur par la quantite de rouge, vert et bleu
+*/  
 struct color {
-    double r,g,b;
+    double r; /*< quantite de rouge */
+    double g; /*< quantite de vert */
+    double b; /*< quantite de bleu */
 }typedef COLOR;
 
+
+//! \class Point
+/*! \brief class represantant un point
+ * 
+ */
 class Point {
 private:
-    double x;
-    double y;
-    COLOR couleur;
+    double x; /*< abcisse du point */
+    double y; /*< ordonné du point */
+    COLOR couleur; /*< couleur du point */
 public:
     friend class Dessin;
+    
+    
+    //! \brief constructeur par default 
+	/*! constructeur par default de la class Point
+	 */
     Point(); 
+    
+    	//! \brief constructeur de la class Point
+		/*!
+		 * \param x : abscisse du point
+		 * \param y : ordonnée du point
+		 * \param r : quantite de rouge pour la couleur du point
+		 * \param g : quantite de vert pour la couleur du point
+		 * \param b : quantite de bleu pour la couleur du point
+		 */
     Point(const double x,const double y, const double r, const double g, const double b); 
+    
+    //! \brief destructeur de la class Point
     ~Point(); 
+    
+     //! \brief accesseur de l'abcisse
     double getPx() const;
+    
+    //! \brief accesseur de l'ordonne
     double getPy() const;
+    
+    //! \brief accesseur de la quantite de rouge
     double getr() const;
+    
+    //! \brief accesseur de la quantite de vert
     double getg() const;
+    
+    //! \brief accesseur de la quantite de bleu
     double getb() const;
+    
+    //! \brief redefinition de l'operateur =
     Point& operator=(const Point& p);
 };
 
--- glWidget.h
+++ glWidget.h
@@ -4,21 +4,38 @@
 #include <QtOpenGL>
 #include <QGLWidget>
 #include "Dessin.hh"
-
+/*!
+ *  \file glWidget.h
+ * \brief class de widget Qt 
+ */
+ 
+ //! \class GLWidget 
+/*! \brief class permettant l'integration d'opengl dans une interface Qt
+cette classe dont la class Opengl herite permet de dessiner avec openGl dans un widget Qt
+*/ 
 class GLWidget : public QGLWidget
 {
-    Q_OBJECT	
+    Q_OBJECT	; /*< objet qt*/
 public:
+	//! \brief constructeur de GLWidget
     explicit GLWidget(int framesPerSecond = 0, QWidget *parent = 0, char *name = 0);
+    
+    //! \brief fonction d'iniatlisation 
     virtual void initializeGL() = 0;
+    
+    //! \brief fonction de redimensionnage 
     virtual void resizeGL(int width, int height) = 0;
+    
+    //! \brief fonction de dessin
     virtual void paintGL() = 0;
    
         
 public slots:
+	//! \brief fonction necessaire à l'integration d'opengl dans qt
     virtual void timeOutSlot();
 
 private:
+	//! \brief attribut necessaire à l'integration d'opengl dans qt
     QTimer *t_Timer;
 
 };
